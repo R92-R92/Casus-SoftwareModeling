e3026a96e7a7efbd9ddcfb5c01aabff64b8928b2
Robbin Kempen <2406543kempen@zuyd.nl>
2025-05-26 17:32:04 +0200

    Classes opgezet

---
+++ StudioManager/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioManager
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+
+        public Address(string street, string houseNumber, string postalCode, string city, string country)
+
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+        }
+    }
+}
---
+++ StudioManager/Prop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioManager
+{
+    public class Prop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsAvaileable { get; set; }
+
+        public Prop(int id, string name, string description, bool isAvaileable)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            IsAvaileable = isAvaileable;
+        }
+    }
+}
