db48247198d062f82920d1ccc2e840a33920f5ea
R92-R92 <2406543kempen@zuyd.nl>
2025-05-31 21:21:33 +0200

    DAL-Read

--- StudioManager/Contract.cs
+++ StudioManager/Contract.cs
@@ -7,24 +7,22 @@ using System.Threading.Tasks;
 
 namespace StudioManager
 {
-    class Contract
+    public class Contract
     {
         public int Id { get; set; }
         public string Body { get; set; }
         public Contact? Signee { get; set; }
         public bool IsSigned { get; set; }
         public DateTime? SignedOn { get; set; }
-        public User Author { get; set; }
         public Shoot? Shoot { get; set; }
 
-        public Contract(int id, string body, Contact? signee, DateTime? signedOn, User author, Shoot? shoot)
+        public Contract(int id, string body, Contact? signee, bool isSigned, DateTime? signedOn, Shoot? shoot)
         {
             Id = id;
             Body = body;
             Signee = signee;
-            IsSigned = false;
+            IsSigned = isSigned;
             SignedOn = signedOn;
-            Author = author;
             Shoot = shoot;
         }
     }
